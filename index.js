b65772754e3bd17ccd9ae0d3182794e4f212cf18
Hana Sambora <grace.sambora@gmail.com>
2023-06-12 18:45:04 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
+const { config } = require("chai");
+
 // Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle"
+    })
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+.then(function(response) {
+    return response.json();
+})
+.then(function(object) {
+    console.log(object);
+})
+.catch(function(error) {
+    alert("Bad things!");
+    console.log(error.message);
+});
+
+function submitData (nameIn, emailIn) {
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: nameIn,
+            email: emailIn
+        })
+    }
+
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        document.body.append(object["id"])
+    }) 
+    .catch(function(error){
+        document.body.append(error.message)
+    })
+}
